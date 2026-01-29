815446938ef874ed09d6c51e54089b4388825b5a
Isaac Njoroge <isaacnj02@gmail.com>
2026-01-29 18:52:19 +0300

    Add gallery images and implement the events slideshow modal

--- components/events/EventsGrid.tsx
+++ components/events/EventsGrid.tsx
@@ -1,7 +1,8 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { EventCard } from "./EventCard";
+import { SlideshowModal } from "./SlideshowModal";
 import { EventType } from "@/types";
 
 interface EventsGridProps {
@@ -9,16 +10,45 @@ interface EventsGridProps {
 }
 
 export const EventsGrid: React.FC<EventsGridProps> = ({ events }) => {
-  const handleEventClick = () => {
-    // TODO: Open modal with event details and gallery
-    // This will be implemented in the next ticket
+  const [selectedEvent, setSelectedEvent] = useState<EventType | null>(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleEventClick = (event: EventType) => {
+    setSelectedEvent(event);
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleBookClick = () => {
+    // TODO: Open booking modal - will be implemented in future tickets
+    setIsModalOpen(false);
+    // Scroll to contact section or open booking form
+    const contactSection = document.getElementById("contact");
+    if (contactSection) {
+      contactSection.scrollIntoView({ behavior: "smooth" });
+    }
   };
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-8">
-      {events.map((event) => (
-        <EventCard key={event.id} event={event} onClick={handleEventClick} />
-      ))}
-    </div>
+    <>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-8">
+        {events.map((event) => (
+          <EventCard key={event.id} event={event} onClick={() => handleEventClick(event)} />
+        ))}
+      </div>
+
+      {/* Slideshow Modal */}
+      {selectedEvent && (
+        <SlideshowModal
+          isOpen={isModalOpen}
+          onClose={handleCloseModal}
+          event={selectedEvent}
+          onBookClick={handleBookClick}
+        />
+      )}
+    </>
   );
 };
